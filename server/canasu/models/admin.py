35bee6614230b70c07928c9f9455c18a08730486
nupur <nupurbhagatpln@gmail.com>
2023-07-01 22:14:33 +0530

    admin schema created

---
+++ server/canasu/models/admin.py
@@ -0,0 +1,18 @@
+from canasu.database import db, ma
+from sqlalchemy import func
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False)
+    email=db.Column(db.String(100), nullable=False)
+    phone=db.Column(db.String(100), nullable=False)
+    password=db.Column(db.String(100), nullable=False)
+    slots=db.Column(db.Integer)
+  
+   
+class AdminSchema(ma.Schema):
+    class Meta:
+        fields = ('id','name', 'email', 'phone', 'password', 'slots')
+                  
+admin_schema = AdminSchema()
