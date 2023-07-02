f9077e4a97e6e8c886876ead829bc58d161b6d9d
koushiks11 <koushiks.is20@bmsce.ac.in>
2023-07-02 05:44:48 +0530

    Working signin/signup and authentication of user using server and client communication

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,39 +1,44 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Dashboard from "./pages/Dashboard"
-import {useState } from 'react'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
 import PrivateRoute from "./components/PrivateRoute";
-
+import AdminDash from "./components/AdminDash";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
- 
 
   return (
     <div className="w-screen min-h-screen bg-pink-900 flex flex-col">
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
       <Routes>
-
-        <Route path="/" element= {<Home isLoggedIn={isLoggedIn}/>} />
-        <Route path="/login" element = {<Login  setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/signup" element={<Signup  setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/dashboard" element = {
-          <PrivateRoute isLoggedIn={isLoggedIn}>
-              <Dashboard/>
-          </PrivateRoute>
-       
-        } />
-
+        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
 
+      {/* <AdminDash /> */}
     </div>
-    )
+  );
 }
 
 export default App;
