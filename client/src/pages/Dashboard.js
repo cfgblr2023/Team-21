4fd30124c3b8b2cc39aa2da821158a1c06893281
Nidhi Sheth <84659956+ShethNidhi06@users.noreply.github.com>
2023-07-02 08:59:11 +0530

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./Dashboard.css";
 
-const Dashboard = () => {
+function Dashboard() {
   return (
-    <div className='flex justify-center items-center text-white text-3xl h-full'>
-      Welcome to Dashboard!!
+    <div className="dashboard">
+      <div className="row">
+        <div className="tile">
+          <div className="tile-content">Siksha</div>
+        </div>
+        <div className="tile">
+          <div className="tile-content">Udaan</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="tile">
+          <div className="tile-content">Vridhi</div>
+        </div>
+        <div className="tile">
+          <div className="tile-content">EmpowHer</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
