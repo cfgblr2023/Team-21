35be72da7454f5a541418cb1a2f161985426b8c6
Nidhi Sheth <84659956+ShethNidhi06@users.noreply.github.com>
2023-07-02 06:20:15 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import ImageSlider from '../components/ImageSlider';
+import { SliderData } from '../components/SliderData';
 const Home = () => {
   return (
     <div className='flex justify-center items-center text-white text-3xl h-full'>
-      Home
+      
+          <ImageSlider slides={SliderData} />;
     </div>
   )
 }
